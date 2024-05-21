11a6e2efc56b48b104d17576e189c06d5edea6c8
armanscott40 <armanzare8298@gmail.com>
2024-05-21 15:46:03 +0330

    commitj2-2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello worlda!");
+        float f1 = 12.3f;
+        float f2 = 12412.512f;
+        float f3 = 1241555552.512f;
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(f3);
+        double d1=123456;
+        double d2=262626.5465;
+        double d3=52552215112.546262626 ;
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println(b1);
+        System.out.println(b2);
+        char c1 = 'a';
+        System.out.println(c1);
+        char c2 = 'a';
+        System.out.println(c2);
+
+        ;
+
 
     }
 }
