797f0f271fc2d3763b35fc0f0829ed8e89a3d805
armanscott40 <armanzare8298@gmail.com>
2024-05-27 08:28:21 +0330

    commitj2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello worlda!");
         String name="Seyed Arman";
-        String family="Zare Estakhreji";
+        String family="Zare Esta khreji";
         int age=29;
         String gender="Male";
         System.out.println("My Name "+name+"\n"+"Family :"+family+"\n"+
