9aa2116247402ef2d349ac164908a684f34b32a3
armanscott40 <armanzare8298@gmail.com>
2024-06-11 09:57:04 +0330

    commitj2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int I = 1;
@@ -7,17 +9,47 @@ public class Main {
         int C = 100;
         int D = 500;
         int M = 1000;
+        Scanner reader=new Scanner(System.in);
+        System.out.println("لطفا عدد یونانی مورد نیاز را وارد کنید ");
+
+        String x = reader.nextLine();
+
         int sum = 0;
-        String str = "CCLX";
-        System.out.println("String is  ="+ str);
-        sum = sum+C+C+L+X;
-        System.out.println("String sum  ="+ sum);
+        for (char ch : x.toCharArray()) {
+
+            int num;
+            switch (ch) {
+                case 'I' :
+                    num=1 ;
+                    break;
+                case 'V':
+                    num=5;
+                    break;
+                case 'X' :
+                    num=10 ;
+                    break;
+                case 'L' :
+                    num=50 ;
+                    break;
+                case 'C' :
+                    num=100 ;
+                    break;
+                case 'D' :
+                    num=500 ;
+                    break;
+                case 'M' :
+                    num=1000 ;
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + ch);
+            }
 
 
+            sum += num;
 
+        }
 
-
-
+        System.out.println("Your Number is " + sum);
 
 
     }
