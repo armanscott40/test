b56bc31381991983da0a2690223074db3ca9e8bd
armanscott40 <armanzare8298@gmail.com>
2024-06-17 16:25:04 +0330

    commitj2

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        int I = 1;
-        int V = 5;
-        int X = 10;
-        int L = 50;
-        int C = 100;
-        int D = 500;
-        int M = 1000;
-        Scanner reader=new Scanner(System.in);
-        System.out.println("لطفا عدد یونانی مورد نیاز را وارد کنید ");
-
-        String x = reader.nextLine();
-
-        int sum = 0;
-        for (char ch : x.toCharArray()) {
-
-            int num;
-            switch (ch) {
-                case 'I' :
-                    num=1 ;
-                    break;
-                case 'V':
-                    num=5;
-                    break;
-                case 'X' :
-                    num=10 ;
-                    break;
-                case 'L' :
-                    num=50 ;
-                    break;
-                case 'C' :
-                    num=100 ;
-                    break;
-                case 'D' :
-                    num=500 ;
-                    break;
-                case 'M' :
-                    num=1000 ;
-                    break;
-                default:
-                    throw new IllegalStateException("Unexpected value: " + ch);
-            }
-
-
-            sum += num;
-
-        }
-
-        System.out.println("Your Number is " + sum);
-
+    public static <string> void main(String[] args) {
+        int a = factorial(5);
+        System.out.println( " نتبجه فاکتوریل " + a);
 
     }
+    public static int factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
 }
