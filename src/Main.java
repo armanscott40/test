595571fb50ebc10f609cb71c59409fa3b4894f38
armanscott40 <armanzare8298@gmail.com>
2024-07-23 09:16:59 +0330

    commitj2

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,7 @@ public class Main {
         Restaurant restaurant = new Restaurant("رستوران آرمان ", "تهران، ناهید شرقی ", 5);
         restaurant.displayInfo();
         restaurant.addMenuItem("پیتزا", 500000, 4.0);
-        restaurant.addMenuItem("برگر", 290000, 3.2);
         restaurant.addMenuItem("سالاد", 2500000, 5.0);
-        restaurant.addMenuItem("پاستا", 39000, 4.7);
         restaurant.addMenuItem("سوپ", 100000, 4.3);
         restaurant.addMenuItem("دسر تیرامیسو ", 28000, 4.1);
         restaurant.displayMenu();
